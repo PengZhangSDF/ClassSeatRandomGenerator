729ce06f2f64702814ff2b3f34f9d53dc00b0ac9
PengZhangSDF <1307717743@qq.com>
2024-12-07 23:34:02 +0800

    2.1 Update

--- utils/setting_utils.py
+++ utils/setting_utils.py
@@ -3,23 +3,22 @@ import subprocess
 import time
 from utils.get_yaml_value import get_yaml_value
 
-def kill_process(process_name, interval=1):
+
+def kill_process(process_name):
     """
     循环执行 taskkill 命令以终止指定的进程。
-
-    :param process_name: 要终止的进程名称
-    :param interval: 每次执行之间的等待时间（秒）
     """
     while True:
-        if get_yaml_value('regulatory_taskkill'):
+        if get_yaml_value("regulatory_taskkill"):
             try:
-                # 执行 taskkill 命令
-                subprocess.run(['taskkill', '/F', '/IM', process_name], check=True)
+                # 执行 taskkill 命令，使用 creationflags 防止窗口闪烁
+                subprocess.run(['taskkill', '/F', '/IM', process_name], check=True,
+                               creationflags=subprocess.CREATE_NO_WINDOW)
                 print(f"已终止进程: {process_name}")
             except subprocess.CalledProcessError:
                 print(f"未找到进程: {process_name}，可能已经被终止。")
 
             # 等待指定的时间
-            time.sleep(interval)
+            time.sleep(1)
 if __name__ == '__main__':
     kill_process("SeewoAbility.exe")
